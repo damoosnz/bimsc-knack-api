ac2ffe259b71fb6d54c23afd37341d75daf83156
damoosnz <damien.legrand@bimsolutionscentre.com>
2025-04-03 14:48:50 +1300

    added config

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import { filters } from "./knack-api/knack-api-filters.js";
 import { calls } from "./knack-api/knack-api-calls.js";
 import { utils } from "./knack-api/knack-api-utils.js";
 import { timeout } from "./knack-api/knack-api-timeout.js";
+import { config } from "./knack-api/knack-api-config.js";
 
 export const knackApi = {
+    config:  config,
     filters: filters,
     payloads: payloads,
     calls: calls,
---
+++ knack-api/knack-api-config.js
@@ -0,0 +1,82 @@
+function createKnackApiConfig(nickName, runEnv, app_id, login, password) {
+    return {
+        [nickName]: {
+            runEnv: runEnv,
+            app_id: app_id,
+            login: login || '',
+            password: password || ''
+        }
+    }
+}
+
+const defaultConfig = {
+    default: {
+        runEnv: process.env.KNACK_API_RUNENV,
+        app_id: process.env.KNACK_APP_ID,
+        login: process.env.KNACK_API_LOGIN || '',
+        password: process.env.KNACK_API_PASSWORD || ''
+    }
+
+}
+
+function initKnackApiConfig(userConfigs = []) {
+
+    let configs = [defaultConfig]
+    if (userConfigs.length > 0)
+        userConfigs.forEach(config => { configs, push(config) });
+    return configs
+
+}
+
+function checkDefaultConfig() {
+
+    // check if the runEnv is defined
+    const runEnv = process.env.KNACK_API_RUNENV
+    if(!runEnv) {
+        console.log('runEnv is not specified')
+        return false
+    }
+
+    if(runEnv !== 'browser' || runEnv !== 'server') {
+        console.log('runEnv must be either browser or server')
+        return false
+    }
+
+    if(runEnv = 'browser') {
+        return true
+    }
+
+    if(runEnv = 'server') {
+        // check application id
+        const applicationId =  process.env.KNACK_APP_ID
+        if(!applicationId) {
+            console.log('application id must be specified')
+            return false
+        }
+
+        const email = process.env.KNACK_API_LOGIN
+        if(!email) {
+            console.log('login must be specified')
+            return false
+        }
+
+        const password = process.env.KNACK_API_PASSWORD
+        if(!password) {
+            console.log('password must be specified')
+            return false
+        }
+
+        return true
+    }
+
+    console.log('config is incorrect')
+    return false
+
+}
+
+
+export const config = {
+    create: (runEnv, app_id, login, password) => createKnackApiConfig(runEnv, app_id, login, password),
+    init: (userConfigs) => initKnackApiConfig(userConfigs),
+    checkDefault: () => checkDefaultConfig()
+}
